4885ccbc06e8cd8d60ad25a42b8f0e16a4f560ee
Ssbas <33584221+Ssbas@users.noreply.github.com>
2022-10-15 11:13:30 -0300

    Create items.py

---
+++ principal/items.py
@@ -0,0 +1,17 @@
+
+class items():
+    def __init__(self):
+        self.p = {
+            "GUANTES": ["AGILIDAD",10],
+            "PECHERA": ["DEFENSA",20],
+            "BOTAS": ["VELOCIDAD",30],
+            "POCION": ["SALUD:",5],
+            "ESPECIAL": ["VELOCIDAD,SALUD:",23],
+        }
+    def get_itm(self):
+        return self.p
+
+
+
+
+emp = items()
